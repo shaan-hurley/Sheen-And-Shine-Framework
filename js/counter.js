664df5eedae8966ec9480165339dc9f4226dd669
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-07-17 03:20:49 -0700

    styled counter component

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,99 @@
+
+const template= document.createElement('template')
+// Set the content of the template
+template.innerHTML = `
+<style>
+    .container {
+        margin: 0px 30px;
+    }
+    h1 {
+        display: absolute;
+        transform: translate3d(174px, 165px, 0px);
+        font-size: 42pt;
+        color: #001E29;
+    }
+
+  .inner {
+    display: flex;
+    transition: 400ms;
+  }
+  button{
+      display: relative
+      padding: 8px;
+      font-size: 14pt;
+      border: none;
+      background-color: #005A7A;
+      font-weight: bold;
+      transition: 500ms;
+  }
+
+  button:hover{
+    background-color: #00B4F5;
+}
+
+    button:focus {
+        outline: none;
+    }
+
+  .triangle {
+    display: absolute;
+    width: 140px;
+    height: 140px;
+    clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+    transform: rotate(-90deg);
+  }
+  .triangle-2 {
+    display: absolute;
+    width: 140px;
+    height: 140px;
+    clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+    transform: translate3d(250px, -227px, 0px)rotate(90deg);
+  }
+  .text-inc {
+    display: absolute;
+    font-size: 14pt;
+    color: #DBF2FA;
+    font-weight: bold;
+    transform: translate3d(30px, -115px, 0);
+  }
+  .text-dec {
+    display: absolute;
+    font-size: 14pt;
+    color: #DBF2FA;
+    font-weight: bold;
+    transform: translate3d(260px, -340px, 0);
+  }
+</style>
+<div class="container">
+    <h1>Counter</h1>
+    <button id='btn-inc' class="triangle"></button>
+    <div class="text-inc">
+        <h4>Increment</h4>
+    </div>
+    <button id='btn-dec' class="triangle-2"></button>
+    <div class="text-dec">
+        <h4>Decrement</h4>
+    </div>
+</div>
+`
+
+
 class Counter extends HTMLElement {
     constructor() {
         super();
-        this.shadow = this.attachShadow({ mode: 'open' })
+        const tempNode = template.content.cloneNode(true);
+        this._shadow = this.attachShadow({ mode: 'open' })
+        this._shadow.appendChild(tempNode)
+        this._h1 = this._shadow.querySelector('h1');
+        this._incButton = this._shadow.querySelector('#btn-inc')
+        this._decButton = this._shadow.querySelector('#btn-dec')
+        this._count = 0
+        this._incButton.addEventListener("click", this.inc.bind(this))
+
+        this._decButton.addEventListener("click", () => {
+            this.dec()
+    
+        })
     }
     get count(){
         return this.getAttribute("count");
@@ -13,7 +105,14 @@ class Counter extends HTMLElement {
         return ["count"];
     }
     inc(){
-        this.count += 1;
+        console.log(this)
+        this._count += 1;
+        this.render()
+    }
+    dec(){
+        console.log(this)
+        this._count -= 1;
+        this.render()
     }
     attributeChangedCallback(prop, oldVal, newVal){
         if(prop === "count"){
@@ -29,13 +128,8 @@ class Counter extends HTMLElement {
     }
 
     render(){
-        this.shadow.innerHTML = `
-            <h1>Counter</h1>
-            ${this.count}
-            <button id='btn'>Increment</button>
-        
-        
-        `;
+        this._h1.innerHTML = this._count
+
     }
 }
 
